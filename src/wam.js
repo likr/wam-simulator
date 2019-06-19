ee59098d864b599323f5abc514f08c079f67f351
Yosuke Onoue <onoue@likr-lab.com>
2019-06-19 15:39:59 +0900

    fix lqm line generation

--- src/wam.js
+++ src/wam.js
@@ -9,12 +9,15 @@ export const wam = (d, params, rk = true) => {
   for (let i = 1; i <= d.length; ++i) {
     if (rk) {
       const k1 = a0 + a1 * d[i - 1] - (b0 + b1 * d[i - 1]) * Ft[i - 1]
-      const k2 = a0 + a1 * d[i - 1] - (b0 + b1 * d[i - 1]) * (Ft[i - 1] + k1 / 2)
-      const k3 = a0 + a1 * d[i - 1] - (b0 + b1 * d[i - 1]) * (Ft[i - 1] + k2 / 2)
+      const k2 =
+        a0 + a1 * d[i - 1] - (b0 + b1 * d[i - 1]) * (Ft[i - 1] + k1 / 2)
+      const k3 =
+        a0 + a1 * d[i - 1] - (b0 + b1 * d[i - 1]) * (Ft[i - 1] + k2 / 2)
       const k4 = a0 + a1 * d[i - 1] - (b0 + b1 * d[i - 1]) * (Ft[i - 1] + k3)
       Ft[i] = Ft[i - 1] + (k1 + 2 * k2 + 2 * k3 + k4) / 6
     } else {
-      Ft[i] = Ft[i - 1] + (a0 + a1 * d[i - 1]) - (b0 + b1 * d[i - 1]) * Ft[i - 1]
+      Ft[i] =
+        Ft[i - 1] + (a0 + a1 * d[i - 1]) - (b0 + b1 * d[i - 1]) * Ft[i - 1]
     }
   }
   return Ft
@@ -24,20 +27,19 @@ export const lqm = (d, params) => {
   const alpha = params.alpha * 1e-5
   const beta = params.beta * 1e-5
   const F0 = params.F0 * 1e-5
-  const Ft = new Array(d.length)
+  const Ft = new Array(d.length + 1)
+  Ft[0] = F0
 
   let bias = F0
   let D = 0
-  for (let i = 0; i < d.length; ++i) {
-    if (d[i] === 0) {
-      if (i > 0) {
-        bias = Ft[i - 1]
-      }
+  for (let i = 1; i <= d.length; ++i) {
+    if (d[i - 1] === 0) {
+      bias = Ft[i - 1]
       D = 0
     }
-    D += d[i]
+    D += d[i - 1]
     if (D === 0) {
-      Ft[i] = i === 0 ? bias : Ft[i - 1]
+      Ft[i] = Ft[i - 1]
     } else {
       Ft[i] = Math.max(0, bias + alpha * D + beta * D * D)
     }
